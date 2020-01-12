8425eb397f575a3c3389ae2fde899f1896645a62
AnnaSnigur <58662898+AnnaSnigur@users.noreply.github.com>
2020-01-12 10:07:46 +0200

    Update views.py

--- students/views.py
+++ students/views.py
@@ -1,1 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
 
+def generate_student(request):
+    student = Student.generate_student()
+    return HttpResponse(f'{student.get_info()}')
